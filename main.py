44c6297626d22644f56d71779dc429f9ceece5c5
amosley72 <amosley@nd.edu>
2026-06-12 16:27:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -227,4 +227,6 @@ async def analyze_strip(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    # This dynamically checks if Render gave it a port, fallback to 8001 if local
+    port = int(os.environ.get("PORT", 8001))
+    uvicorn.run(app, host="0.0.0.0", port=port)
