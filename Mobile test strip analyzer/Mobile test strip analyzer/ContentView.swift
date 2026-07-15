813f31f4864e1a273daf1b3c1f5682720fa65601
amosley72 <amosley@nd.edu>
2026-07-15 16:02:31 -0400

    Save restored backend graph logic and frontend dart updates

---
+++ Mobile test strip analyzer/Mobile test strip analyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mobile test strip analyzer
+//
+//  Created by Afton Mosley on 7/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
